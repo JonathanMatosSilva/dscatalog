53adaf584bdb049e39814f93b04fba2637d07f35
jonathan.silva <jonathan.msdev@gmail.com>
2025-02-04 20:57:17 -0300

    H2 database

---
+++ backend/src/main/java/com/jonathan/dscatalog/services/CategoryService.java
@@ -0,0 +1,19 @@
+package com.jonathan.dscatalog.services;
+
+import com.jonathan.dscatalog.entities.Category;
+import com.jonathan.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+}
