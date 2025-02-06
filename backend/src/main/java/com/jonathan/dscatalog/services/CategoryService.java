0f51e07a3e6ecaf79a831ebf36823c4c99d2ff2a
jonathan.silva <jonathan.msdev@gmail.com>
2025-02-05 21:50:15 -0300

    Delete Category

--- backend/src/main/java/com/jonathan/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/jonathan/dscatalog/services/CategoryService.java
@@ -3,10 +3,13 @@ package com.jonathan.dscatalog.services;
 import com.jonathan.dscatalog.dto.CategoryDTO;
 import com.jonathan.dscatalog.entities.Category;
 import com.jonathan.dscatalog.repositories.CategoryRepository;
+import com.jonathan.dscatalog.services.exceptions.DatabaseException;
 import com.jonathan.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -50,4 +53,18 @@ public class CategoryService {
             throw new ResourceNotFoundException("Id not found " + id);
         }
     }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+
 }
